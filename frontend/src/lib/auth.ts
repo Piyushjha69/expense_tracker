c46b7605c7a6975e3f6585ae23f32a19c8b9b47a
Piyushjha69 <piyushjhatkd@gmail.com>
2026-02-08 22:10:00 +0530

    login page added

---
+++ frontend/src/lib/auth.ts
@@ -0,0 +1,28 @@
+const ACCESS_KEY = "access_token"
+const REFRESH_KEY = "refresh_token"
+
+export function setAccessToken(token: string) {
+    localStorage.setItem(ACCESS_KEY, token)
+}
+
+export function getAccessToken() {
+    return localStorage.getItem(ACCESS_KEY)
+}
+
+export function setRefreshToken(token: string) {
+    localStorage.setItem(REFRESH_KEY, token)
+}
+
+export function getRefreshToken() {
+    return localStorage.getItem(REFRESH_KEY)
+}
+
+export function logout() {
+    localStorage.removeItem(ACCESS_KEY)
+    localStorage.removeItem(REFRESH_KEY)
+
+    //Redirect to login page
+    if(typeof window !== undefined) {
+        window.location.href="/login"
+    }
+}
